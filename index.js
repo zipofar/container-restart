14b6381beef57ff900a5f8e8e732b979b2ec2d96
fedorov-dualboot <110913994+fedorov-dualboot@users.noreply.github.com>
2022-11-14 17:30:35 +0400

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const HOST = "0.0.0.0";
 const app = express();
 app.get("/", (_, res) => {
   console.log("request to /");
-  res.send("Hello ver 4");
+  res.send("Hello world ver 5");
 });
 
 app.get("/exit", () => {
